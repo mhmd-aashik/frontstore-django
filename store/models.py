ab33915a1fcc5019f53088f8d88b58d3a75c63c6
mhmd-aashik <aashikdevelop@gmail.com>
2024-09-22 19:40:16 +0530

    Defining one-to-one Relationship Bug corrected

--- store/models.py
+++ store/models.py
@@ -43,4 +43,4 @@ class Order(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE,primary_key=True)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE,primary_key=True)
